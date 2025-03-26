a82ad7481cff06d0113afa05f2871d48a048cf73
Vitor de Paula Gomes <112875256+VitorPgms@users.noreply.github.com>
2025-03-25 21:32:57 -0300

    Finalizando tela de login e criando tela de cadastro

--- app/src/main/java/com/example/appdefilmes/view/FormLogin.kt
+++ app/src/main/java/com/example/appdefilmes/view/FormLogin.kt
@@ -1,5 +1,6 @@
 package com.example.appdefilmes.view
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,12 +8,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.appdefilmes.R
+import com.example.appdefilmes.databinding.ActivityFormLoginBinding
 
 class FormLogin : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityFormLoginBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_form_login)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -20,6 +26,28 @@ class FormLogin : AppCompatActivity() {
         }
 
         window.statusBarColor = Color.parseColor("#000000")
+        binding.editEmail.requestFocus()
+
+        binding.btnEnter.setOnClickListener {
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            when{
+                email.isEmpty() -> {
+                    binding.containerEmail.helperText = "Preencha o email"
+                    binding.containerEmail.boxStrokeColor = Color.parseColor("#FF5722")
+                }
+                senha.isEmpty() -> {
+                    binding.containerSenha.helperText = "Preencha a senha"
+                    binding.containerSenha.boxStrokeColor = Color.parseColor("#FF5722")
+                }
+            }
+        }
+
+        binding.txtScreenCadastre.setOnClickListener {
+            val intent = Intent(this, FormCadastro::class.java)
+            startActivity(intent)
+        }
 
     }
 }
