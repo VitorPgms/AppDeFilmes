51a70886ac8412de9071ce97bce2c8a83df6c015
Vitor de Paula Gomes <112875256+VitorPgms@users.noreply.github.com>
2025-03-31 21:57:27 -0300

    tela de cadastro: Logica e criando tela.

--- app/src/main/java/com/example/appdefilmes/view/FormCadastro.kt
+++ app/src/main/java/com/example/appdefilmes/view/FormCadastro.kt
@@ -1,21 +1,65 @@
 package com.example.appdefilmes.view
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.appdefilmes.R
+import com.example.appdefilmes.databinding.ActivityFormCadastroBinding
 
 class FormCadastro : AppCompatActivity() {
+    private lateinit var binding: ActivityFormCadastroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityFormCadastroBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_form_cadastro)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        window.statusBarColor = Color.parseColor("#FFFFFF")
+        binding.editEmail.requestFocus()
+
+        binding.btVamosLa.setOnClickListener {
+            val email = binding.editEmail.text.toString()
+
+            if (!email.isEmpty()){
+                binding.containerSenha.visibility = View.VISIBLE
+                binding.btVamosLa.visibility = View.GONE
+                binding.btContinuar.visibility = View.VISIBLE
+                binding.txtTitulo.setText("Um mundo de séries e filmes \n ilimitados espera por você.")
+                binding.txtDescricao.setText("Crie uma conta para saber mais sobre \n nosso App de Filmes ")
+                binding.containerEmail.boxStrokeColor = Color.parseColor("#4CAF50FF")
+                binding.containerEmail.helperText = ""
+                binding.containerHeader.visibility = View.VISIBLE
+            } else {
+                binding.containerEmail.boxStrokeColor = Color.parseColor("#FF0000")
+                binding.containerEmail.helperText = "O email é obrigatorio."
+            }
+        }
+
+        binding.btContinuar.setOnClickListener {
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            if(!email.isEmpty() && !senha.isEmpty()){
+                Toast.makeText(this, "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT).show()
+            } else if (senha.isEmpty()){
+                binding.containerSenha.boxStrokeColor = Color.parseColor("#FF0000")
+                binding.containerSenha.helperText = "A senha é obrigatoria."
+                binding.containerEmail.boxStrokeColor = Color.parseColor("#4CAF50FF")
+            } else if (email.isEmpty()){
+                binding.containerEmail.boxStrokeColor = Color.parseColor("#FF0000")
+                binding.containerEmail.helperText = "O email é obrigatorio."
+            }
         }
     }
 }
