67d55ff29b9843675af46f47976e6e22e8609e37
Vitor de Paula Gomes <112875256+VitorPgms@users.noreply.github.com>
2025-04-24 11:36:58 -0300

    Finalizando Aplicativo. Telas e consumo de API: Ok

--- app/src/main/java/com/example/appdefilmes/view/DetalhesFilme.kt
+++ app/src/main/java/com/example/appdefilmes/view/DetalhesFilme.kt
@@ -1,21 +1,44 @@
 package com.example.appdefilmes.view
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.example.appdefilmes.R
+import com.example.appdefilmes.databinding.ActivityDetalhesFilmeBinding
 
 class DetalhesFilme : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetalhesFilmeBinding
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityDetalhesFilmeBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detalhes_filme)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        window.statusBarColor = Color.parseColor("#000000")
+
+        val capa = intent.extras?.getString("capa")
+        val nome = intent.extras?.getString("nome")
+        val descricao = intent.extras?.getString("descricao")
+        val elenco = intent.extras?.getString("elenco")
+
+        Glide.with(this).load(capa).centerCrop().into(binding.capaFilme)
+        binding.txtNome.setText(nome)
+        binding.txtDescricao.setText("Descrição: $descricao")
+        binding.txtElenco.setText("Elenco: $elenco")
+
     }
 }
